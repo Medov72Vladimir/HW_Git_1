0fcf17d26038654a3a5b70499a9b12b08ce8e659
Medov72Vladimir <vlmedov@mail.ru>
2023-11-09 22:11:57 +0300

    д/з к семинару 4

---
+++ Homework_4/Pearson_formula.py
@@ -0,0 +1,23 @@
+import random
+
+def average_value(arr):
+    return sum(arr) / len(arr)
+
+# Функция вычисления корреляции Пирсона
+def correlation(x, y):
+    average_x = average_value(x)
+    average_y = average_value(y)
+
+    dividend = sum((x[i] - average_x) * (y[i] - average_y) for i in range(len(x)))
+    divider = (sum((xi - average_x)**2 for xi in x) * sum((yi - average_y)**2 for yi in y)) ** 0.5
+
+    return dividend / divider
+
+# Генерируем два массива для тестирования
+x = [random.randint(1, 10) for _ in range(10)]
+y = [random.randint(1, 10) for _ in range(10)]
+print("x = ", x)
+print("y = ", y)
+
+# Вызываем функцию для расчета корреляции Пирсона и выводим результат
+print(correlation(x, y))
