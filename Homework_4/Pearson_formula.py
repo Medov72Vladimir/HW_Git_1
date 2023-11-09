b81c9a053a2acfed2984c2384a31e6778d4e2ed2
Medov72Vladimir <vlmedov@mail.ru>
2023-11-09 22:15:18 +0300

    д/з к семинару 4

--- Homework_4/Pearson_formula.py
+++ Homework_4/Pearson_formula.py
@@ -1,5 +1,6 @@
 import random
 
+# Функция вычисления среднего значения массива
 def average_value(arr):
     return sum(arr) / len(arr)
 
@@ -7,13 +8,11 @@ def average_value(arr):
 def correlation(x, y):
     average_x = average_value(x)
     average_y = average_value(y)
-
     dividend = sum((x[i] - average_x) * (y[i] - average_y) for i in range(len(x)))
     divider = (sum((xi - average_x)**2 for xi in x) * sum((yi - average_y)**2 for yi in y)) ** 0.5
-
     return dividend / divider
 
-# Генерируем два массива для тестирования
+# Генерируем два массива
 x = [random.randint(1, 10) for _ in range(10)]
 y = [random.randint(1, 10) for _ in range(10)]
 print("x = ", x)
